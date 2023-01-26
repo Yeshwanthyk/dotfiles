af971de18012f1e909872cebbe9797d65e5ad0c4
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2023-01-26 00:06:34 -0500

    Add tokyonight as colorscheme for nvim

--- .config/nvim/lua/hsey/base.lua
+++ .config/nvim/lua/hsey/base.lua
@@ -2,7 +2,7 @@ vim.cmd("autocmd!")
 
 vim.g.mapleader = ","
 
-vim.cmd [[ colorscheme tokyonight-night ]]
+vim.cmd [[ colorscheme tokyonight ]]
 
 vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
