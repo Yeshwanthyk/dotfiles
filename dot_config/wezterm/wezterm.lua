3109f310768e6d8145a78db3b4d8a6d16cdcdce5
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2024-02-13 17:02:19 -0500

    Add wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,31 +9,42 @@
 -- https://wezfurlong.org/wezterm/
 
 local k = require("utils/keys")
+local gpu_adapters = require("utils.gpu_adapter")
+local colors = require("colors.custom")
 
 local wezterm = require("wezterm")
 local act = wezterm.action
 
 return {
 
-	font_size = 18,
+	font_size = 19,
+	animation_fps = 60,
+	max_fps = 60,
+	front_end = "WebGpu",
+	webgpu_power_preference = "HighPerformance",
+	webgpu_preferred_adapter = gpu_adapters:pick(),
 
 	-- Color scheme
 	-- https://wezfurlong.org/wezterm/config/appearance.html
-	window_background_opacity = 1,
 	initial_cols = 150,
 	initial_rows = 40,
-	color_scheme = "Tokyo Night",
+	-- color_scheme = "Tokyo Night",
+	-- window_background_opacity = 0.95,
+	colors = colors,
 
+	-- window
 	window_padding = {
-		left = 20,
-		right = 20,
-		top = 20,
-		bottom = 10,
+		left = 5,
+		right = 10,
+		top = 12,
+		bottom = 7,
 	},
 
 	-- Font configuration
 	-- https://wezfurlong.org/wezterm/config/fonts.html
-	font = wezterm.font("Hack Nerd Font Mono"),
+	font = wezterm.font(
+		"BerkeleyMono Nerd Font Mono Plus Font Awesome Plus Font Awesome Extension Plus Octicons Plus Power Symbols Plus Codicons Plus Pomicons Plus Font Logos Plus Material Design Icons Plus Weather Icons"
+	),
 
 	-- general options
 	adjust_window_size_when_changing_font_size = false,
@@ -44,19 +55,19 @@ return {
 	window_decorations = "RESIZE",
 
 	keys = {
-		k.cmd_key(".", k.multiple_actions(":ZenMode")),
-		k.cmd_key("[", act.SendKey({ mods = "CTRL", key = "o" })),
-		k.cmd_key("]", act.SendKey({ mods = "CTRL", key = "i" })),
-		k.cmd_key("f", k.multiple_actions(":Grep")),
-		k.cmd_key("H", act.SendKey({ mods = "CTRL", key = "h" })),
-		k.cmd_key("i", k.multiple_actions(":SmartGoTo")),
-		k.cmd_key("J", act.SendKey({ mods = "CTRL", key = "j" })),
-		k.cmd_key("K", act.SendKey({ mods = "CTRL", key = "k" })),
-		k.cmd_key("L", act.SendKey({ mods = "CTRL", key = "l" })),
-		k.cmd_key("P", k.multiple_actions(":GoToCommand")),
-		k.cmd_key("p", k.multiple_actions(":GoToFile")),
-		k.cmd_key("j", k.multiple_actions(":GoToFile")),
-		k.cmd_key("q", k.multiple_actions(":qa!")),
+		-- k.cmd_key(".", k.multiple_actions(":ZenMode")),
+		-- k.cmd_key("[", act.SendKey({ mods = "CTRL", key = "o" })),
+		-- k.cmd_key("]", act.SendKey({ mods = "CTRL", key = "i" })),
+		-- k.cmd_key("f", k.multiple_actions(":Grep")),
+		-- k.cmd_key("H", act.SendKey({ mods = "CTRL", key = "h" })),
+		-- k.cmd_key("i", k.multiple_actions(":SmartGoTo")),
+		-- k.cmd_key("J", act.SendKey({ mods = "CTRL", key = "j" })),
+		-- k.cmd_key("K", act.SendKey({ mods = "CTRL", key = "k" })),
+		-- k.cmd_key("L", act.SendKey({ mods = "CTRL", key = "l" })),
+		-- k.cmd_key("P", k.multiple_actions(":GoToCommand")),
+		-- k.cmd_key("p", k.multiple_actions(":GoToFile")),
+		-- k.cmd_key("j", k.multiple_actions(":GoToFile")),
+		-- k.cmd_key("q", k.multiple_actions(":qa!")),
 		k.cmd_to_tmux_prefix("1", "1"),
 		k.cmd_to_tmux_prefix("2", "2"),
 		k.cmd_to_tmux_prefix("3", "3"),
