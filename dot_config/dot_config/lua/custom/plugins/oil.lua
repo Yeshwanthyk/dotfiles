dd17425c058106c25ef70df58ac10129fdced756
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2024-08-13 09:56:40 -0400

    Updated kickstart

--- dot_config/dot_config/lua/custom/plugins/oil.lua
+++ dot_config/dot_config/lua/custom/plugins/oil.lua
@@ -142,4 +142,30 @@ return {
     -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
+
+  -- Neo-tree is a Neovim plugin to browse the file system
+  -- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    cmd = 'Neotree',
+    keys = {
+      { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    },
+    opts = {
+      filesystem = {
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+          },
+        },
+      },
+    },
+  },
 }
